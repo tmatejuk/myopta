41da6af208f9f929a520edf47fcc88fb610be9ca
tomek <tomek@nb>
2023-09-07 10:46:02 +0200

    calculator for eny 2 numbers

---
+++ user_calculator.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return (x+y)
+
+print("Which numbers do you want to add?")
+x = int(input("give first number : "))
+y = int(input("give secont number : "))
+print("sum is : ", add(x,y))
+
