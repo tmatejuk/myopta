a43eafc9f1d1733ae3a7be522daa8e8dbe79598c
tomek <tomek@nb>
2023-09-08 10:53:18 +0200

    calculator z sprawdzaniem podanych wartosci

--- user_calculator.py
+++ user_calculator.py
@@ -2,8 +2,19 @@ def add(x,y):
     return (x+y)
 
 while True:
+    x = 0
+    y = 0
+    
     print("Which numbers do you want to add?")
-    x = int(input("give first number : "))
-    y = int(input("give secont number : "))
+    try:
+        x = int(input("give first number : "))
+    except:
+        print("podanej wartosci nie mozna zamienic na liczbe")
+
+    try:
+        y = int(input("give first number : "))
+    except:
+        print("podanej wartosci nie mozna zamienic na liczbe")
+
     print("sum is : ", add(x,y))
 
